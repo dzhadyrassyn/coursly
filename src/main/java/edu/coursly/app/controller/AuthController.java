cf6a7738d3a9f36c08a80dffea7e3671d16f175a
Daniyar Zhadyrassyn <zhadyrassyn.daniyar@gmail.com>
2025-10-13 23:55:40 +0500

    Add AuthController mockup for register and login methods

---
+++ src/main/java/edu/coursly/app/controller/AuthController.java
@@ -0,0 +1,18 @@
+package edu.coursly.app.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @PostMapping
+    public void register() {
+
+    }
+
+    @PostMapping
+    public void login() {
+
+    }
+}
