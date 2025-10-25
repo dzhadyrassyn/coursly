9f6c0bdf967650d37044b4bd0ffc4c655a1e0b57
Daniyar Zhadyrassyn <zhadyrassyn.daniyar@gmail.com>
2025-10-25 23:33:45 +0500

    Enable cors for frontend

---
+++ backend/src/main/java/edu/coursly/app/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package edu.coursly.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
