84ea1161f5c25fbb3bf155af8e4c9da649bee8ea
Daniyar Zhadyrassyn <zhadyrassyn.daniyar@gmail.com>
2025-10-26 23:46:11 +0500

    Add proxy in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/login': 'http://localhost:8080',
+      '/register': 'http://localhost:8080',
+      '/refresh': 'http://localhost:8080'
+    }
+  }
 })
