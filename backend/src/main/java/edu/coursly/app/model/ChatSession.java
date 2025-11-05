301e988665c0c260d96a376b37374da2934960dc
Daniyar Zhadyrassyn <zhadyrassyn.daniyar@gmail.com>
2025-11-05 23:58:50 +0500

    Add ChatSession model

---
+++ backend/src/main/java/edu/coursly/app/model/ChatSession.java
@@ -0,0 +1,23 @@
+package edu.coursly.app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "chat_session")
+public class ChatSession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+}
