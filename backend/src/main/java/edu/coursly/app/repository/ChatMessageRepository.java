35d332304bce935f4cb98067044ad2155d727ab5
Daniyar Zhadyrassyn <zhadyrassyn.daniyar@gmail.com>
2025-11-06 00:30:42 +0500

    Add ChatMessageRepository and ChatSessionRepository

---
+++ backend/src/main/java/edu/coursly/app/repository/ChatMessageRepository.java
@@ -0,0 +1,8 @@
+package edu.coursly.app.repository;
+
+import edu.coursly.app.model.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {}
---
+++ backend/src/main/java/edu/coursly/app/repository/ChatSessionRepository.java
@@ -0,0 +1,8 @@
+package edu.coursly.app.repository;
+
+import edu.coursly.app.model.ChatSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {}
